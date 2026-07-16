103de9c0850f62fd830ff561eea6c022b98b1b0b
Douglas <dougv8.427@gmail.com>
2026-07-16 14:52:49 -0300

    chore: upgrade `cookie` to latest  version

--- infra/controller.js
+++ infra/controller.js
@@ -6,7 +6,7 @@ import {
   UnauthorizedError,
   ForbiddenError,
 } from "infra/errors.js";
-import * as cookie from "cookie";
+import { stringifySetCookie } from "cookie";
 import session from "models/session.js";
 import user from "models/user.js";
 import authorization from "models/authorization.js";
@@ -37,7 +37,9 @@ function onErrorHandler(error, request, response) {
 }
 
 function setSessionCookie(sessionToken, response) {
-  const setCookie = cookie.serialize("session_id", sessionToken, {
+  const setCookie = stringifySetCookie({
+    name: "session_id",
+    value: sessionToken,
     path: "/",
     maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
     secure: process.env.NODE_ENV === "production",
@@ -49,7 +51,9 @@ function setSessionCookie(sessionToken, response) {
 }
 
 function clearSessionCookie(response) {
-  const setCookie = cookie.serialize("session_id", "invalid", {
+  const setCookie = stringifySetCookie({
+    name: "session_id",
+    value: "invalid",
     path: "/",
     maxAge: -1,
     secure: process.env.NODE_ENV === "production",
